aa2ba79a3d72800374577c5c6f1d6779711ecd93
cos4338@gmail.com <cos4338@gmail.com>
2023-04-03 23:50:34 +0100

    Feat: feature paramSlice by using RTK to handle issue params when fetching issue list

--- src/store/slices/paramSlice.ts
+++ src/store/slices/paramSlice.ts
@@ -1,17 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialIssueParams = {
+  page: 1,
+  per_page: 10,
+  sort: 'updated',
+  direction: 'asc',
+  state: 'open',
+};
+
 const paramSlice = createSlice({
   name: 'params',
-  initialState: {
-    issueParams: {
-      page: 1,
-      per_page: 20,
-      sort: 'updated',
-      direction: 'asc',
+
+  initialState: { issueParams: initialIssueParams },
+
+  reducers: {
+    updateIssueParams: (state, action) => {
+      state.issueParams = { ...state.issueParams, ...action.payload };
     },
   },
-  reducers: {},
 });
 
-export const {} = paramSlice.actions;
+export const { updateIssueParams } = paramSlice.actions;
 export default paramSlice.reducer;
